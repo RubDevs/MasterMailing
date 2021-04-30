8b8d5230e4753fb3177b530cd97550cc274c6309
Ruben Hernandez <ruben.hernandez.puerta@rubdevs.com>
2021-04-30 14:00:50 -0500

    middleware created

--- api/components/subscriber/network.js
+++ api/components/subscriber/network.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 const Controller = require("./index");
+const validation = require("../../middlewares/validation");
 
 router.post("/register", function (req, res) {
   const { subscriber } = req.body;
@@ -12,6 +13,11 @@ router.post("/register", function (req, res) {
     .catch((error) => {
       res.status(400).send({ error: error.message });
     });
+});
+
+router.post("/send", validation, (req, res) => {
+  //Request to Mailchimp API
+  res.send("Validated");
 });
 
 module.exports = router;
