cf6ec1b75d780180814a04fd89b699624b5c0997
Ruben Hernandez <ruben.hernandez.puerta@rubdevs.com>
2021-04-30 12:02:08 -0500

    controller added

---
+++ api/components/subscriber/index.js
@@ -0,0 +1,15 @@
+const controller = require("./controller");
+const config = require("../../../config");
+
+let store;
+
+if (config.dev) {
+  //Dummy when development or test
+  store = require("../../../lib/dummy");
+} else {
+  //Mongo when production
+  store = require("../../../lib/mongo");
+}
+
+//Dependency Injection
+module.exports = controller(store);
