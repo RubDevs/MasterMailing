511bcd4d1e53fa9de123704a6e7a35302460910c
Ruben Hernandez <ruben.hernandez.puerta@rubdevs.com>
2021-04-30 12:49:56 -0500

    component working

--- api/components/subscriber/controller.js
+++ api/components/subscriber/controller.js
@@ -1,11 +1,18 @@
 const { save } = require("../../../lib/mongo");
 const Model = require("../../../models/subscriber");
+const config = require("../../../config");
+const jwt = require("jsonwebtoken");
 
 module.exports = function (injectedStore) {
   let store = injectedStore;
 
   async function save(subscriber) {
-    return store.save(Model, subscriber);
+    try {
+      await store.save(Model, subscriber);
+      return jwt.sign({ email: subscriber.email }, config.jwt.secret);
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   return {
---
+++ api/components/subscriber/network.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const router = express.Router();
+const Controller = require("./index");
+
+router.post("/register", function (req, res) {
+  const { subscriber } = req.body;
+  Controller.save(subscriber)
+    .then((token) => {
+      res.status(200).send({ token });
+    })
+    .catch((error) => {
+      res.status(400).send({ error: "Bad request" });
+    });
+});
+
+module.exports = router;
