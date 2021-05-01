51830b5bd88db26a08a1c90ada55138eb7ec11f4
Rubén Hernández <69823633+RubDevs@users.noreply.github.com>
2021-05-01 09:05:18 -0500

    cors enabled (#23)

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const subscriber = require("./api/components/subscriber/network");
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/subscriber", subscriber);
 
 app.listen(config.api.port, () => {
